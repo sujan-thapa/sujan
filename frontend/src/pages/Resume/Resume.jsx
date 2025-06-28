d4902b7bba270d75cba0fbbe14400a5adc51b56f
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-06-28 17:45:35 -0400

    resume-updated

--- frontend/src/pages/Resume/Resume.jsx
+++ frontend/src/pages/Resume/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     return (
         <div className={styles.resume}>
             <h2>RESUME</h2>
-            <p>Full Resume can be found <span><a href="https://drive.google.com/file/d/1HW-CMdmdHH3g-An5bzeCnIiOftERKvJg/view?usp=drive_link">here</a></span>.</p>
+            <p>Full Resume can be found <span><a href="https://docs.google.com/document/d/1FNNG41mBwVy8Y1wRLGWer3HciWSkimb1/edit?usp=sharing&ouid=115605567566538738230&rtpof=true&sd=true">here</a></span>.</p>
         </div>
     );
 };
