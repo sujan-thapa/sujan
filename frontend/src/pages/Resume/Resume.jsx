0c27bb6a478ffc3a39376ed24378f3af4761191f
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-07-03 08:03:34 -0400

    Resume page updated

--- frontend/src/pages/Resume/Resume.jsx
+++ frontend/src/pages/Resume/Resume.jsx
@@ -3,10 +3,47 @@ import styles from './Resume.module.css';
 
 const Resume = () => {
     return (
-        <div className={styles.resume}>
+        <main className={styles.resume}>
             <h2>RESUME</h2>
-            <p>Full Resume can be found <span><a href="https://docs.google.com/document/d/1FNNG41mBwVy8Y1wRLGWer3HciWSkimb1/edit?usp=sharing&ouid=115605567566538738230&rtpof=true&sd=true">here</a></span>.</p>
-        </div>
+            {/* Embedded Google Doc */}
+            <iframe
+                src="https://docs.google.com/document/d/1FNNG41mBwVy8Y1wRLGWer3HciWSkimb1/preview"
+                width="100%"
+                height="400px"
+                style={{ border: "none", marginTop: "2rem" }}
+                title="Resume"
+            ></iframe>
+            {/* Download Button */}
+            <p>
+                <a
+                    href="https://docs.google.com/document/d/1FNNG41mBwVy8Y1wRLGWer3HciWSkimb1/export?format=pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.downloadBtn}
+                >
+                    Download PDF
+                </a>
+            </p>
+            {/* Key Sections */}
+            <section>
+                <h3>Technical Skills</h3>
+                <h4>Languages and Frameworks</h4>
+                <ul>
+                    <li>Languages: JavaScript, Python, Java</li>
+                    <li>Frameworks/Libraries: React.js, Next.js, Node.js</li>
+                    <li>Markup & Styling: HTML5, CSS3</li>
+                    {/* Add more skills */}
+                </ul>
+                <h4>Tools and Platforms</h4>
+                <ul>
+                    <li>Git/GitHub, VS Code, Postman</li>
+                    <li>Microsoft Office, Google Docs/Sheets</li>
+                    <li>Familiar with Docker & Linux terminal</li>
+                    {/* Add more skills */}
+                </ul>
+            </section>
+            
+        </main>
     );
 };
 
