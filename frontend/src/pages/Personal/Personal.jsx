40d359e532afda1b5aa53497b82712392e9ce354
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-07-24 03:10:36 -0400

    Personal Page update

--- frontend/src/pages/Personal/Personal.jsx
+++ frontend/src/pages/Personal/Personal.jsx
@@ -21,7 +21,7 @@ const Personal = () => {
                     <p>I am a big fan of football. I enjoy watching matches, following my favorite teams, and playing the sport whenever I get the chance. Football inspires me with its teamwork, strategy, and passion.</p>
 
                 </div>
-                <div>
+                <div className={styles.imagesContainer}>
                     <img className={styles.images} src={football1} alt="Football1" />
                     <img className={styles.images} src={football2} alt="Football2" />
                 </div>
@@ -32,7 +32,7 @@ const Personal = () => {
                 <h2>Traveling</h2>
                 <p>I love to travel and explore new places. Traveling allows me to experience different cultures, meet new people, and gain fresh perspectives.</p>
                 </div>
-                <div>
+                <div className={styles.imagesContainer}>
                     <img className={styles.images} src={travel1} alt="Traveling1" />
                     <img className={styles.images} src={travel2} alt="Traveling2" />
                 </div>
